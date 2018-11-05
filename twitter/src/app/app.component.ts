9b9237fa269ab8e4b023707b5761d12d7fc1d3af
Courtney <chalt0319@gmail.com>
2018-11-05 09:43:42 -0500

    add input to create post

--- twitter/src/app/app.component.ts
+++ twitter/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'twitter';
+  postArray=[]
+
+  addPost(value){
+    this.postArray.push(value)
+    console.log(this.postArray)
+  }
+
 }
